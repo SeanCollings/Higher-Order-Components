8ca34b982fd1af321ec3e5733d424a29e736df80
collingss <nightharrier@gmail.com>
2018-01-11 07:03:36 +0200

    HOC Implementation - Authentication

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    // must declare which context types are required first
+    // before they can be used
+    // then can access router from within i.e. this.context.router
+    static contextTypes = {
+      // get access to router (react-router) from anywhere in the app by using
+      // Authentication.contextTypes
+      router: React.PropTypes.object
+    };
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authneticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
+      // console.log(this.props.authenticated);
+
       return <ComposedComponent {...this.props} />;
     }
   }
 
-  return Authentication;
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
